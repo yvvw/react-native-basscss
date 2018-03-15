a505d22a112208902341b120854c1de69b4741eb
dongdayu <g592842897@gmail.com>
2018-03-15 10:29:55 +0800

    add border module.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import flexbox from './modules/flexbox'
 import layout from './modules/layout'
 import position from './modules/position'
+import border from './modules/border'
 import hide from './modules/hide'
 
 module.exports = {
   ...flexbox,
   ...layout,
   ...position,
+  ...border,
   ...hide,
 }
---
+++ modules/border.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  borderTopNone: { borderTopWidth: 0 },
+  borderRightNone: { borderRightWidth: 0 },
+  borderBottomNone: { borderBottomWidth: 0 },
+  borderLeftNone: { borderLeftWidth: 0 },
+  borderNone: {
+    borderTopWidth: 0,
+    borderRightWidth: 0,
+    borderBottomWidth: 0,
+    borderLeftWidth: 0
+  },
+})
